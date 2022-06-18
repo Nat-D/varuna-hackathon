776b9d98bc47b5818e4defd9481f29e849484e40
NataphopT <42538419+NataphopT@users.noreply.github.com>
2022-06-18 18:46:31 +0700

    clean test.py for submission

--- test.py
+++ test.py
@@ -52,55 +52,20 @@ def test_prediction_csv(pred_np, test_mask_dir, output_dir):
     df = pd.DataFrame(data = sorted_mask_pred_np, columns = column_values).astype(int)
     df.to_csv(output_dir, index = False)  
 
-def all_prediction_csv(pred_np, train_mask_dir, output_dir):
-
-    # get prediction in each polygon
-    mask_pred_np = np.zeros((0, 3))
-    for mask_dir in train_mask_dir:
-        for crop_type in ['1','2','3','4']:
-            all_mask = os.listdir(mask_dir+crop_type)
-            print(mask_dir + crop_type)
-            for mask in all_mask:
-
-                mask_np = np.load(mask_dir + crop_type + '/' + mask).astype(np.float32)
-                values, counts = np.unique(mask_np*pred_np, return_counts=True)
-                sorted_index = sorted(range(len(counts)), key=lambda k: counts[k])
-
-                max_value = values[sorted_index[-2]]
-                poly_index = int(mask.replace(".npy", ""))
-                label = int(crop_type)
-
-                mask_pred_np = np.vstack([mask_pred_np, np.array([poly_index, max_value, label]).astype(int)])
-
-    sorted_mask_pred_np = mask_pred_np[mask_pred_np[:, 0].argsort()]
-
-    # creating a list of column names
-    column_values = ['index', 'crop_type', 'label']
-    df = pd.DataFrame(data = sorted_mask_pred_np, columns = column_values).astype(int)
-    df.to_csv(output_dir, index = False)  
-
 if __name__ == "__main__":
 
-    PATH = 'runs/small-training-reweight-nobatchnorm-elastic/my_checkpoint.pth.tar'
+    PATH = 'runs/3500/3500.tar'
     DEVICE = 'cpu'
     PREPROCESS = Standardize('PS', device= DEVICE)
     MODEL = NoNameUNET(in_channels=12, out_channels=5, preprocess=PREPROCESS).to(DEVICE)
     SELECTED_DAYS = [20210106]
-    TEST = False
     TEST_DATA_DIR = './test_data/'
-    TRAIN_MASK_DIR = ['./train_output/mask/', './val_output/mask/']
     TEST_MASK_DIR = './test_output/mask/'
-    OUTPUT_DIR = './testing_output.csv' if TEST else './training_output.csv'
+    OUTPUT_DIR = './output.csv'
     
-
     model = MODEL
     model.load_state_dict(torch.load(PATH, map_location = torch.device(DEVICE))["state_dict"])
-    # model = load_checkpoint(torch.load(PATH, map_location = torch.device(DEVICE)), MODEL)
     model.eval()  # model in testing model
 
     pred_np = predictions(model, SELECTED_DAYS, TEST_DATA_DIR, DEVICE)
-
-    if TEST:
-        test_prediction_csv(pred_np, TEST_MASK_DIR, OUTPUT_DIR)
-    else:
-        all_prediction_csv(pred_np, TRAIN_MASK_DIR, OUTPUT_DIR)
+    test_prediction_csv(pred_np, TEST_MASK_DIR, OUTPUT_DIR)
